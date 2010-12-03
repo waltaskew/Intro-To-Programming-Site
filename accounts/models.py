7cd02f47cb1bee63019ffa3c61cdda59ecaa2104
Walt Askew <waltaskew@gmail.com>
2010-12-02 21:56:56 -0600

    Add is_answered method; Link answers to UserProfile's rather than to Users

--- accounts/models.py
+++ accounts/models.py
@@ -25,7 +25,7 @@ class UserProfile(models.Model):
     def add_answer(self, problem, program=None):
         """Add an answer to a problem for the user.
         """
-        answer, created = Answer.objects.get_or_create(answered_by=self.user,
+        answer, created = Answer.objects.get_or_create(answered_by=self,
                                                        problem=problem)
         if created:
             self.total_answered += 1
@@ -37,7 +37,14 @@ class UserProfile(models.Model):
     def get_answered_problems(self):
         """Return the problems that the user has answered.
         """
-        return Problem.objects.all().filter(answer__answered_by=self.user)
+        return Problem.objects.all().filter(answer__answered_by=self)
+
+    def is_answered(self, problem):
+        """Return a boolean idicating whether a given problem
+        has been solved by the user.
+        """
+        return bool(Answer.objects.all().filter(answered_by=self,
+                                                problem=problem))
 
     def save(self, *args, **kwargs):
         """Initialiaze the created count."""
@@ -50,10 +57,10 @@ class Answer(models.Model):
     """
     created = models.DateField(auto_now=True, auto_now_add=True)
     problem = models.ForeignKey(Problem)
-    answered_by = models.ForeignKey(User)
+    answered_by = models.ForeignKey(UserProfile)
     program = models.FileField(upload_to=get_path, null=True, blank=True)
 
     def __unicode__(self):
         """String representation of the instance.
         """
-        return '%s answered by %s' % (self.problem, self.answered_by)
+        return '%s answered by %s' % (self.problem, self.answered_by.user)
