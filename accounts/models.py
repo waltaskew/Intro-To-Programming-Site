7a299bb8ba1574f62e3268f90ea4bc0be6878ca9
Walt Askew <waltaskew@gmail.com>
2010-12-08 21:52:33 -0600

    Create a secret key for created users; remove unused get_path function

--- accounts/models.py
+++ accounts/models.py
@@ -1,28 +1,24 @@
-import os.path
+import random
 
 from django.contrib.auth.models import User
 from django.db import models
 
 from problems.models import Problem
 
-def get_path(instance, filename):
-    """Return a path for the user to upload files to.
-    """
-    username = instance.answered_by.user.username
-    return os.path.join('programs', username, filename)
 
 class UserProfile(models.Model):
     """User profile for the user accounts.
     """
     user = models.OneToOneField(User)
     total_answered = models.IntegerField()
+    secret_key = models.CharField(max_length=32)
 
     def __unicode__(self):
         """String representation of the instance.
         """
         return 'User Profile for %s' % self.user
 
-    def add_answer(self, problem, program=None):
+    def add_answer(self, problem):
         """Add an answer to a problem for the user.
         """
         answer, created = Answer.objects.get_or_create(answered_by=self,
@@ -30,9 +26,6 @@ class UserProfile(models.Model):
         if created:
             self.total_answered += 1
             self.save()
-        if program is not None:
-            answer.program = program
-        return answer.save()
 
     def get_answered_problems(self):
         """Return the problems that the user has answered.
@@ -49,6 +42,8 @@ class UserProfile(models.Model):
     def save(self, *args, **kwargs):
         """Initialiaze the created count."""
         if not self.id:
+            random.seed()
+            self.secret_key = '%016x' % random.getrandbits(128)
             self.total_answered = 0
         return super(UserProfile, self).save(*args, **kwargs)
 
@@ -58,7 +53,6 @@ class Answer(models.Model):
     created = models.DateField(auto_now=True, auto_now_add=True)
     problem = models.ForeignKey(Problem, related_name='given_answer')
     answered_by = models.ForeignKey(UserProfile)
-    program = models.FileField(upload_to=get_path, null=True, blank=True)
 
     def __unicode__(self):
         """String representation of the instance.
