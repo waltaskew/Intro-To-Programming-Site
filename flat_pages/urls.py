39eb092f55fe3d75c08feada5f116c961d7b3a57
Walt Askew <waltaskew@gmail.com>
2010-12-23 17:37:02 -0500

    update slug regex to allow hyphens; add hypens to slugs in fixture

--- flat_pages/urls.py
+++ flat_pages/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('flat_pages.views',
-        url(r'^(?P<slug>\w+)/$', 
+        url(r'^(?P<slug>[\w|-]+)/$', 
             'detail', 
             {'template_name': 'flat_pages/detail.html'},
             name='flat_pages_detail'),
